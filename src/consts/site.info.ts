981268f698a9008cea897bc1b972878f77602ae2
sebahernandez <sebaprogramer@gmail.com>
2024-08-25 11:17:39 -0400

    actualizacion de contenidos y info sites

---
+++ src/consts/site.info.ts
@@ -0,0 +1,5 @@
+export const siteInfo = {
+  title: "Automotive Consulting - Venta de vehiculos usados",
+  description:
+    "Venta de vehículos usados de las mejores marcas y modelos. Contamos con financiamiento y garantía en todos nuestros vehículos.",
+};
